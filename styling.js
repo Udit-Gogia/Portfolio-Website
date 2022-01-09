1325eeef760ff848a1a7870e93d179f2865d481a
Gogia_Udit_17 <88316529+Udit-Gogia@users.noreply.github.com>
2022-01-09 21:54:32 +0530

    JS Simplified

--- styling.js
+++ styling.js
@@ -1,3 +1,5 @@
+window.onscroll = function(){Navbar("#e66d42")};
+
 function ColorIcons(rang){
     document.getElementById('edu-logo').src = `images/edu/edu-${rang}.png`;
     document.getElementById('aim-logo').src = `images/aim/aim-${rang}.png`;
@@ -13,47 +15,56 @@ function ColorIcons(rang){
 
 function applygreen(){
     document.querySelector(':root').style = " --default: #30E3CA";
-    window.onscroll = function(){greennavbar()};
+    window.onscroll = function(){Navbar("#30E3CA")};
     ColorIcons("green");
+    Navbar("#30E3CA");
+
+
 }
 function applyblue(){
     document.querySelector(':root').style = " --default: #82ACFF";
-    window.onscroll = function(){bluenavbar()};
+    window.onscroll = function(){Navbar("#82ACFF")};
     ColorIcons("blue");
+    Navbar("#82ACFF");
+
 }
 function applypurple(){
     document.querySelector(':root').style = " --default: #892CDC";
-    window.onscroll = function(){purplenavbar()};
+    window.onscroll = function(){Navbar("#892CDC")};
     ColorIcons("purple");
+    Navbar("#892CDC");
+
 }
 function applyorange(){
     document.querySelector(':root').style = " --default: #e66d42";
-    window.onscroll = function(){orangenavbar()};
+    window.onscroll = function(){Navbar("#e66d42")};
     ColorIcons("orng");
+    Navbar("#e66d42");
+
 }
 
-window.onscroll = function(){orangenavbar()};
+window.onscroll = function(){Navbar("#e66d42")};
 
-function orangenavbar(){
+function Navbar(rang){
     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
 
         document.getElementById('nav1').style.transition = "color 1s, font-size 1s";
-        document.getElementById('nav1').style.color = "#e66d42";
+        document.getElementById('nav1').style.color = `${rang}`;
         document.getElementById('nav1').style.fontSize = "14px";
         document.getElementById('nav1').style.textShadow = "0 1px 0 white";
 
         document.getElementById('nav2').style.transition = "color 1s, font-size 1s";
-        document.getElementById('nav2').style.color = "#e66d42";
+        document.getElementById('nav2').style.color = `${rang}`;
         document.getElementById('nav2').style.fontSize = "14px";
         document.getElementById('nav2').style.textShadow = "0 1px 0 white";
 
         document.getElementById('nav3').style.transition = "color 1s, font-size 1s";
-        document.getElementById('nav3').style.color = "#e66d42";
+        document.getElementById('nav3').style.color = `${rang}`;
         document.getElementById('nav3').style.fontSize = "14px";
         document.getElementById('nav3').style.textShadow = "0 1px 0 white";
 
         document.getElementById('nav4').style.transition = "color 1s, font-size 1s";
-        document.getElementById('nav4').style.color = "#e66d42";
+        document.getElementById('nav4').style.color = `${rang}`;
         document.getElementById('nav4').style.fontSize = "14px";
         document.getElementById('nav4').style.textShadow = "0 1px 0 white";
 
@@ -77,135 +88,7 @@ function orangenavbar(){
 
     }
 }
-function bluenavbar(){
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
 
-        document.getElementById('nav1').style.transition = "color 1s, font-size 1s";
-        document.getElementById('nav1').style.color = "#82ACFF";
-        document.getElementById('nav1').style.fontSize = "14px";
-        document.getElementById('nav1').style.textShadow = "0 1px 0 white";
-
-        document.getElementById('nav2').style.transition = "color 1s, font-size 1s";
-        document.getElementById('nav2').style.color = "#82ACFF";
-        document.getElementById('nav2').style.fontSize = "14px";
-        document.getElementById('nav2').style.textShadow = "0 1px 0 white";
-
-        document.getElementById('nav3').style.transition = "color 1s, font-size 1s";
-        document.getElementById('nav3').style.color = "#82ACFF";
-        document.getElementById('nav3').style.fontSize = "14px";
-        document.getElementById('nav3').style.textShadow = "0 1px 0 white";
-
-        document.getElementById('nav4').style.transition = "color 1s, font-size 1s";
-        document.getElementById('nav4').style.color = "#82ACFF";
-        document.getElementById('nav4').style.fontSize = "14px";
-        document.getElementById('nav4').style.textShadow = "0 1px 0 white";
-
-    }
-    else{
-        document.getElementById('nav1').style.transition = "color 1s, font-size 1s";
-        document.getElementById('nav1').style.color = "white";
-        document.getElementById('nav1').style.fontSize = "18px";
-
-        document.getElementById('nav2').style.transition = "color 1s, font-size 1s";
-        document.getElementById('nav2').style.color = "white";
-        document.getElementById('nav2').style.fontSize = "18px";
-
-        document.getElementById('nav3').style.transition = "color 1s, font-size 1s";
-        document.getElementById('nav3').style.color = "white";
-        document.getElementById('nav3').style.fontSize = "18px";
-
-        document.getElementById('nav4').style.transition = "color 1s, font-size 1s";
-        document.getElementById('nav4').style.color = "white";
-        document.getElementById('nav4').style.fontSize = "18px";
-
-    }
-}
-function purplenavbar(){
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-
-        document.getElementById('nav1').style.transition = "color 1s, font-size 1s";
-        document.getElementById('nav1').style.color = "#892CDC";
-        document.getElementById('nav1').style.fontSize = "14px";
-        document.getElementById('nav1').style.textShadow = "0 1px 0 white";
-
-        document.getElementById('nav2').style.transition = "color 1s, font-size 1s";
-        document.getElementById('nav2').style.color = "#892CDC";
-        document.getElementById('nav2').style.fontSize = "14px";
-        document.getElementById('nav2').style.textShadow = "0 1px 0 white";
-
-        document.getElementById('nav3').style.transition = "color 1s, font-size 1s";
-        document.getElementById('nav3').style.color = "#892CDC";
-        document.getElementById('nav3').style.fontSize = "14px";
-        document.getElementById('nav3').style.textShadow = "0 1px 0 white";
-
-        document.getElementById('nav4').style.transition = "color 1s, font-size 1s";
-        document.getElementById('nav4').style.color = "#892CDC";
-        document.getElementById('nav4').style.fontSize = "14px";
-        document.getElementById('nav4').style.textShadow = "0 1px 0 white";
-
-    }
-    else{
-        document.getElementById('nav1').style.transition = "color 1s, font-size 1s";
-        document.getElementById('nav1').style.color = "white";
-        document.getElementById('nav1').style.fontSize = "18px";
-
-        document.getElementById('nav2').style.transition = "color 1s, font-size 1s";
-        document.getElementById('nav2').style.color = "white";
-        document.getElementById('nav2').style.fontSize = "18px";
-
-        document.getElementById('nav3').style.transition = "color 1s, font-size 1s";
-        document.getElementById('nav3').style.color = "white";
-        document.getElementById('nav3').style.fontSize = "18px";
-
-        document.getElementById('nav4').style.transition = "color 1s, font-size 1s";
-        document.getElementById('nav4').style.color = "white";
-        document.getElementById('nav4').style.fontSize = "18px";
-
-    }
-}
-function greennavbar(){
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-
-        document.getElementById('nav1').style.transition = "color 1s, font-size 1s";
-        document.getElementById('nav1').style.color = "#30E3CA";
-        document.getElementById('nav1').style.fontSize = "14px";
-        document.getElementById('nav1').style.textShadow = "0 1px 0 white";
-
-        document.getElementById('nav2').style.transition = "color 1s, font-size 1s";
-        document.getElementById('nav2').style.color = "#30E3CA";
-        document.getElementById('nav2').style.fontSize = "14px";
-        document.getElementById('nav2').style.textShadow = "0 1px 0 white";
-
-        document.getElementById('nav3').style.transition = "color 1s, font-size 1s";
-        document.getElementById('nav3').style.color = "#30E3CA";
-        document.getElementById('nav3').style.fontSize = "14px";
-        document.getElementById('nav3').style.textShadow = "0 1px 0 white";
-
-        document.getElementById('nav4').style.transition = "color 1s, font-size 1s";
-        document.getElementById('nav4').style.color = "#30E3CA";
-        document.getElementById('nav4').style.fontSize = "14px";
-        document.getElementById('nav4').style.textShadow = "0 1px 0 white";
-
-    }
-    else{
-        document.getElementById('nav1').style.transition = "color 1s, font-size 1s";
-        document.getElementById('nav1').style.color = "white";
-        document.getElementById('nav1').style.fontSize = "18px";
-
-        document.getElementById('nav2').style.transition = "color 1s, font-size 1s";
-        document.getElementById('nav2').style.color = "white";
-        document.getElementById('nav2').style.fontSize = "18px";
-
-        document.getElementById('nav3').style.transition = "color 1s, font-size 1s";
-        document.getElementById('nav3').style.color = "white";
-        document.getElementById('nav3').style.fontSize = "18px";
-
-        document.getElementById('nav4').style.transition = "color 1s, font-size 1s";
-        document.getElementById('nav4').style.color = "white";
-        document.getElementById('nav4').style.fontSize = "18px";
-
-    }
-}
 
 
 function proj1link() {
